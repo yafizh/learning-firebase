56e42e2f48a6ad81d60fb54e1918cc8efb859b19
yafizh <a.chairil.yafizh@gmail.com>
2024-02-25 18:33:54 +0700

    deleting data

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ const renderCafe = (doc) => {
     const li = document.createElement("li");
     const name = document.createElement("span");
     const city = document.createElement("span");
+    const cross = document.createElement("div");
 
     li.setAttribute("data-id", doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = 'x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
 
     cafeList.appendChild(li);
+
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    });
 }
 
 db.collection('cafes').get().then((snapshot) => {
