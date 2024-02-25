126ef27d4582ed7519b3645e7f8c553c4324b56c
yafizh <a.chairil.yafizh@gmail.com>
2024-02-25 20:04:04 +0700

    making queries (where and order)

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const cafeList = document.querySelector("#cafe-list");
 const form = document.querySelector("#add-cafe-form");
 
+const formFilter = document.querySelector("#filter-cafe-form");
+const formOrder = document.querySelector("#order-cafe-form");
+
+let filter = {};
+let order = {};
+
 const renderCafe = (doc) => {
     const li = document.createElement("li");
     const name = document.createElement("span");
@@ -25,11 +31,42 @@ const renderCafe = (doc) => {
     });
 }
 
-db.collection('cafes').get().then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-        renderCafe(doc);
+const getCafes = () => {
+    let query = db.collection('cafes')
+
+    if (Object.hasOwn(filter, 'name') || Object.hasOwn(filter, 'city')) {
+        if (Object.hasOwn(filter, 'name'))
+            query = query.where('name', '==', filter.name);
+
+        if (Object.hasOwn(filter, 'city'))
+            query = query.where('city', '==', filter.city);
+    }
+
+    if (Object.hasOwn(order, 'by') && Object.hasOwn(order, 'type'))
+        query = query.orderBy(order.by, order.type);
+
+
+    const initialData = (cafeList.textContent == '');
+    cafeList.textContent = '';
+    query.get().then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            renderCafe(doc);
+
+            if (initialData) {
+                const optionName = document.createElement("option");
+                optionName.textContent = doc.data().name;
+                optionName.value = doc.data().name;
+
+                const optionCity = document.createElement("option");
+                optionCity.textContent = doc.data().city;
+                optionCity.value = doc.data().city;
+
+                formFilter['filter-name'].appendChild(optionName);
+                formFilter['filter-city'].appendChild(optionCity);
+            }
+        });
     });
-});
+}
 
 form.addEventListener('submit', (e) => {
     db.collection('cafes').add({
@@ -40,3 +77,24 @@ form.addEventListener('submit', (e) => {
     form.city.value = '';
     e.preventDefault();
 });
+
+formFilter.addEventListener('submit', (e) => {
+    if (formFilter['filter-name'].value)
+        filter['name'] = formFilter['filter-name'].value;
+
+    if (formFilter['filter-city'].value)
+        filter['city'] = formFilter['filter-city'].value;
+
+    getCafes();
+    e.preventDefault();
+});
+
+formOrder.addEventListener('submit', (e) => {
+    order['by'] = formOrder['order-by'].value;
+    order['type'] = formOrder['order-type'].value;
+
+    getCafes();
+    e.preventDefault();
+});
+
+getCafes();
