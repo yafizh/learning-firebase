cd56f71b779d0669c3d5732062d0958b82e004d5
yafizh <a.chairil.yafizh@gmail.com>
2024-02-25 21:30:49 +0700

    updating data (filter not updated)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const cafeList = document.querySelector("#cafe-list");
 const form = document.querySelector("#add-cafe-form");
+const formEdit = document.querySelector("#edit-cafe-form");
 
 const formFilter = document.querySelector("#filter-cafe-form");
 const formOrder = document.querySelector("#order-cafe-form");
@@ -7,27 +8,49 @@ const formOrder = document.querySelector("#order-cafe-form");
 let filter = {};
 let order = {};
 
+const editModal = document.getElementById("editModal");
+
+window.onclick = function (event) {
+    if (event.target == editModal) {
+        editModal.style.display = "none";
+    }
+}
+
 const renderCafe = (doc) => {
     const li = document.createElement("li");
     const name = document.createElement("span");
     const city = document.createElement("span");
-    const cross = document.createElement("div");
+    const action = document.createElement("div");
+    const cross = document.createElement("span");
+    const edit = document.createElement("span");
 
     li.setAttribute("data-id", doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
     cross.textContent = 'x';
+    edit.textContent = 'e';
 
     li.appendChild(name);
     li.appendChild(city);
-    li.appendChild(cross);
+
+    action.appendChild(cross);
+    action.appendChild(edit);
+
+    li.appendChild(action);
 
     cafeList.appendChild(li);
 
     cross.addEventListener('click', (e) => {
         e.stopPropagation();
-        const id = e.target.parentElement.getAttribute('data-id');
+        const id = e.target.parentElement.parentElement.getAttribute('data-id');
         db.collection('cafes').doc(id).delete();
+    });
+
+    edit.addEventListener('click', (e) => {
+        editModal.style.display = "block";
+        formEdit.setAttribute('data-id', e.target.parentElement.parentElement.getAttribute('data-id'));
+        formEdit.name.value = doc.data().name;
+        formEdit.city.value = doc.data().city;
     });
 }
 
@@ -60,6 +83,19 @@ form.addEventListener('submit', (e) => {
     });
     form.name.value = '';
     form.city.value = '';
+    e.preventDefault();
+});
+
+formEdit.addEventListener('submit', (e) => {
+    const id = formEdit.getAttribute('data-id');
+
+    // set overidde
+    // update just change value that we send
+    db.collection('cafes').doc(id).update({
+        name: formEdit.name.value,
+        city: formEdit.city.value
+    });
+    editModal.style.display = "none";
     e.preventDefault();
 });
 
@@ -103,8 +139,12 @@ db.collection('cafes').onSnapshot((snapshot) => {
                 formFilter['filter-city'].appendChild(optionCity);
             }
         } else if (change.type == 'removed') {
-            let li = cafeList.querySelector(`[data-id=${change.doc.id}]`);
+            let li = cafeList.querySelector(`[data-id='${change.doc.id}']`);
             cafeList.removeChild(li);
+        } else if (change.type == 'modified') {
+            let li = cafeList.querySelector(`[data-id='${change.doc.id}']`);
+            cafeList.removeChild(li);
+            renderCafe(change.doc);
         }
     });
 })
